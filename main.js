b2c0e6191c6b24f168a48f225b74525fd403645f
yajat23 <89627077+yajat23@users.noreply.github.com>
2022-07-11 15:26:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+song="";
+
+function preloade()
+{
+
+    song = loadSound("music.mp3");
+}
+
+function setup()
+{
+canvas = createCanvas(600, 500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+}
+
+function draw()
+{
+ image(video , 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+}
